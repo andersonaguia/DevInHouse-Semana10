b6972f3bb24c888142d237e76a9c6f55db769622
andersonaguiar <54728366+andersonaguia@users.noreply.github.com>
2022-09-03 10:41:13 -0300

    [M1S10] Ex 2 - Trazendo o HTML para JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Home } from './pages/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <h1>Em construção</h1>
+      <Home />
     </div>
   )
 }
